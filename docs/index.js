d952e7325b77f377c89ae4af29c97bf8f2d942ce
ItisNoMatter <702sunnylife@gmail.com>
2021-10-24 17:52:27 +0900

    いったん完成

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,37 @@
-import * as JoyCon from '../node_modules/joy-con-webhid/dist/index.js';
-alert("Hello World!");
+import { connectJoyCon, connectedJoyCons, JoyConLeft } from '../node_modules/joy-con-webhid/dist/index.js';
+const connect_button = document.querySelector('#connect-joy-cons')
+const X_button = document.querySelector('#x_button')
+const Y_button = document.querySelector('#y_button')
+const A_button = document.querySelector('#a_button')
+const B_button = document.querySelector('#b_button')
+
+connect_button.addEventListener('click',connectJoyCon)
+
+
+const visualize = (jouCon,packet) => {
+  
+    if (!packet || !packet.actualOrientation) {
+        return;
+      }
+    const buttons = packet.buttonStatus
+    X_button.classList.toggle("highlight",buttons.x)
+    Y_button.classList.toggle("highlight",buttons.y)
+    A_button.classList.toggle("highlight",buttons.a)
+    B_button.classList.toggle("highlight",buttons.b)
+}
+
+setInterval(async () => {
+    for (const joyCon of connectedJoyCons.values()) {
+      if (joyCon.eventListenerAttached) {
+        continue;
+      }
+      await joyCon.open();
+      await joyCon.enableStandardFullMode();
+      await joyCon.enableIMUMode();
+      await joyCon.enableVibration();
+      joyCon.addEventListener('hidinput', (event) => {
+        visualize(joyCon,event.detail)
+      });
+      joyCon.eventListenerAttached = true;
+    }
+  }, 2000);
