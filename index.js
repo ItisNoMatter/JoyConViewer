592f85e8cca0c6dfdc7bf94f8db674fafbe9dd01
ItisNoMatter <702sunnylife@gmail.com>
2021-10-29 21:28:27 +0900

    commit

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const X_button = document.querySelector('#x_button')
 const Y_button = document.querySelector('#y_button')
 const A_button = document.querySelector('#a_button')
 const B_button = document.querySelector('#b_button')
+const UP_button = document.querySelector('#up_button')
+const Left_button = document.querySelector('#left_button')
+const Right_button = document.querySelector('#right_button')
+const Down_button = document.querySelector('#down_button')
 
-const ctx = document.querySelector('#myChart')
 
-let gyro_x_value
-let gyro_y_value
-let gyro_z_value
+let gyro_x_valueR
+let gyro_y_valueR
+let gyro_z_valueR
+let gyro_x_valueL
+let gyro_y_valueL
+let gyro_z_valueL
+
 
 connect_button.addEventListener('click',connectJoyCon)
 
@@ -20,26 +27,23 @@ let elapsed_time = 0
 let last_called_time = 0
 
 const visualize = (joyCon,packet) => {
-  
     if (!packet || !packet.actualOrientation) {
         return;
       }
     const buttons = packet.buttonStatus
-    const joystick = packet.analogStickRight
 
     X_button.classList.toggle("highlight",buttons.x)
     Y_button.classList.toggle("highlight",buttons.y)
     A_button.classList.toggle("highlight",buttons.a)
     B_button.classList.toggle("highlight",buttons.b)
 
-    const gyroscope = packet.actualGyroscope
-    const gyroMultiplier = 100
-    gyro_x_value = gyroscope.rps.x * gyroMultiplier
-    gyro_y_value = gyroscope.rps.y * gyroMultiplier
-    gyro_z_value = gyroscope.rps.z * gyroMultiplier
+    UP_button.classList.toggle("highlight",buttons.up)
+    Right_button.classList.toggle("highlight",buttons.right)
+    Left_button.classList.toggle("highlight",buttons.left)
+    Down_button.classList.toggle("highlight",buttons.down)
 
     //シェイク検出
-    if (Math.abs(gyro_y_value) > 0.99){
+    if (Math.abs(gyro_y_valueR) > 0.99){
       elapsed_time = Date.now() - last_called_time
       if (elapsed_time > 1000){
         counter_value += 1
@@ -49,8 +53,26 @@ const visualize = (joyCon,packet) => {
     counter.innerHTML = counter_value
 
     const joystickMultiplier = 10
-    document.querySelector('#stickR').style.transform = `translateX(${
+    if (joyCon instanceof JoyConLeft){
+      const joystick =  packet.analogStickLeft
+      document.querySelector('#stickL').style.transform = `translateX(${
       joystick.horizontal * joystickMultiplier    }px) translateY(${joystick.vertical * joystickMultiplier}px)`
+      const gyroscope = packet.actualGyroscope
+      const gyroMultiplier = 100
+      gyro_x_valueR = gyroscope.rps.x * gyroMultiplier
+      gyro_y_valueR = gyroscope.rps.y * gyroMultiplier
+      gyro_z_valueR = gyroscope.rps.z * gyroMultiplier
+
+    }else{
+      const joystick =  packet.analogStickRight
+      document.querySelector('#stickR').style.transform = `translateX(${
+      joystick.horizontal * joystickMultiplier    }px) translateY(${joystick.vertical * joystickMultiplier}px)`
+      const gyroscope = packet.actualGyroscope
+      const gyroMultiplier = 100
+      gyro_x_valueL = gyroscope.rps.x * gyroMultiplier
+      gyro_y_valueL = gyroscope.rps.y * gyroMultiplier
+      gyro_z_valueL = gyroscope.rps.z * gyroMultiplier
+    }
 }
 
 setInterval(async () => {
@@ -71,73 +93,137 @@ setInterval(async () => {
 
   
   
-  const config = {
-    type: 'line',
-    data: {
-      datasets: [
-        { 
-          label: "gyro_x",
-          data: [],
-          pointRadius: 1,
-          borderColor: 'rgb(255, 0, 0)'
-        },
-        { 
-          label: "gyro_y",
-          data: [],
-          pointRadius: 1,
-          borderColor: 'rgb(0, 255, 0)'
-        },
-        { 
-          label: "gyro_z",
-          data: [],
-          pointRadius: 1,
-          borderColor: 'rgb(0, 0, 255)'
-        },
-      ]
-    },
-    responsive:true,
-    
-    options: {
-      layout: {
-        padding: {
-            left: 0,
-            right: 200,
-            top: 0,
-            bottom: 0
-        }
+const config = {
+  type: 'line',
+  data: {
+    datasets: [
+      { 
+        label: "gyro_x",
+        data: [],
+        pointRadius: 1,
+        borderColor: 'rgb(255, 0, 0)'
       },
-      scales: {
-        x: {
-          type: 'realtime',
-          realtime: {
-            delay:1,
-            duration:20000,
-            refresh:1,
-            onRefresh: chart => {
-              
-              chart.data.datasets[0].data.push({
-                x: Date.now(),
-                y: gyro_x_value
-              })
-              chart.data.datasets[1].data.push({
-                x: Date.now(),
-                y: gyro_y_value
-              })
-              chart.data.datasets[2].data.push({
-                x: Date.now(),
-                y: gyro_z_value
-              })
-
-              
-
-            }
+      { 
+        label: "gyro_y",
+        data: [],
+        pointRadius: 1,
+        borderColor: 'rgb(0, 255, 0)'
+      },
+      { 
+        label: "gyro_z",
+        data: [],
+        pointRadius: 1,
+        borderColor: 'rgb(0, 0, 255)'
+      },
+    ],
+  },
+  responsive:true,
+  
+  options: {
+    layout: {
+      padding: {
+          left: 0,
+          right: 200,
+          top: 0,
+          bottom: 0
+      }
+    },
+    scales: {
+      x: {
+        type: 'realtime',
+        realtime: {
+          delay:1,
+          duration:10000,
+          refresh:1,
+          onRefresh: chart => {
+            
+            chart.data.datasets[0].data.push({
+              x: Date.now(),
+              y: gyro_x_valueR
+            })
+            chart.data.datasets[1].data.push({
+              x: Date.now(),
+              y: gyro_y_valueR
+            })
+            chart.data.datasets[2].data.push({
+              x: Date.now(),
+              y: gyro_z_valueR
+            })
           }
         }
       }
     }
-  };
+  }
+};
+const configL = {
+  type: 'line',
+  data: {
+    datasets: [
+      { 
+        label: "gyro_x",
+        data: [],
+        pointRadius: 1,
+        borderColor: 'rgb(255, 0, 0)'
+      },
+      { 
+        label: "gyro_y",
+        data: [],
+        pointRadius: 1,
+        borderColor: 'rgb(0, 255, 0)'
+      },
+      { 
+        label: "gyro_z",
+        data: [],
+        pointRadius: 1,
+        borderColor: 'rgb(0, 0, 255)'
+      },
+    ],
+  },
+  responsive:true,
+  
+  options: {
+    layout: {
+      padding: {
+          left: 0,
+          right: 200,
+          top: 0,
+          bottom: 0
+      }
+    },
+    scales: {
+      x: {
+        type: 'realtime',
+        realtime: {
+          delay:1,
+          duration:10000,
+          refresh:1,
+          onRefresh: chart => {
+            
+            chart.data.datasets[0].data.push({
+              x: Date.now(),
+              y: gyro_x_valueL
+            })
+            chart.data.datasets[1].data.push({
+              x: Date.now(),
+              y: gyro_y_valueL
+            })
+            chart.data.datasets[2].data.push({
+              x: Date.now(),
+              y: gyro_z_valueL
+            })
+          }
+        }
+      }
+    }
+  }
+};
 
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+
+const myChart = new Chart(
+  document.getElementById('myChartR'),
+  config
+);
+const myChart2 = new Chart(
+  document.getElementById('myChartL'),
+  configL
+);
