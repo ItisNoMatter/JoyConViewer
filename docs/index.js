6d0e6b7c414967563f57c2c87f16c2dc5948180c
ItisNoMatter <702sunnylife@gmail.com>
2021-10-29 19:34:18 +0900

    commit

--- docs/index.js
+++ docs/index.js
@@ -5,18 +5,21 @@ const Y_button = document.querySelector('#y_button')
 const A_button = document.querySelector('#a_button')
 const B_button = document.querySelector('#b_button')
 
+const ctx = document.querySelector('#myChart')
 
-const gyro_x = document.querySelector('#gyro_x')
-const gyro_y = document.querySelector('#gyro_y')
-const gyro_z = document.querySelector('#gyro_z')
+let gyro_x_value
+let gyro_y_value
+let gyro_z_value
 
 connect_button.addEventListener('click',connectJoyCon)
+
 const counter = document.querySelector('#counter')
 let counter_value = 0
 
+let elapsed_time = 0
+let last_called_time = 0
 
-
-const visualize = (jouCon,packet) => {
+const visualize = (joyCon,packet) => {
   
     if (!packet || !packet.actualOrientation) {
         return;
@@ -28,17 +31,23 @@ const visualize = (jouCon,packet) => {
     Y_button.classList.toggle("highlight",buttons.y)
     A_button.classList.toggle("highlight",buttons.a)
     B_button.classList.toggle("highlight",buttons.b)
-    
 
     const gyroscope = packet.actualGyroscope
     const gyroMultiplier = 100
-    gyro_x.value = gyroscope.rps.x * gyroMultiplier
-    gyro_y.value = gyroscope.rps.y * gyroMultiplier
-    gyro_z.value = gyroscope.rps.z * gyroMultiplier
-    
-    const THRESHOLD = 0.8
-    count_shake(gyroscope.rps.y * gyroMultiplier,THRESHOLD)
+    gyro_x_value = gyroscope.rps.x * gyroMultiplier
+    gyro_y_value = gyroscope.rps.y * gyroMultiplier
+    gyro_z_value = gyroscope.rps.z * gyroMultiplier
+
+    //シェイク検出
+    if (Math.abs(gyro_y_value) > 0.99){
+      elapsed_time = Date.now() - last_called_time
+      if (elapsed_time > 1000){
+        counter_value += 1
+        last_called_time = Date.now()
+      }
+    }
     counter.innerHTML = counter_value
+
     const joystickMultiplier = 10
     document.querySelector('#stickR').style.transform = `translateX(${
       joystick.horizontal * joystickMultiplier    }px) translateY(${joystick.vertical * joystickMultiplier}px)`
@@ -59,3 +68,76 @@ setInterval(async () => {
       joyCon.eventListenerAttached = true;
     }
   }, 2000);
+
+  
+  
+  const config = {
+    type: 'line',
+    data: {
+      datasets: [
+        { 
+          label: "gyro_x",
+          data: [],
+          pointRadius: 1,
+          borderColor: 'rgb(255, 0, 0)'
+        },
+        { 
+          label: "gyro_y",
+          data: [],
+          pointRadius: 1,
+          borderColor: 'rgb(0, 255, 0)'
+        },
+        { 
+          label: "gyro_z",
+          data: [],
+          pointRadius: 1,
+          borderColor: 'rgb(0, 0, 255)'
+        },
+      ]
+    },
+    responsive:true,
+    
+    options: {
+      layout: {
+        padding: {
+            left: 0,
+            right: 200,
+            top: 0,
+            bottom: 0
+        }
+      },
+      scales: {
+        x: {
+          type: 'realtime',
+          realtime: {
+            delay:1,
+            duration:20000,
+            refresh:1,
+            onRefresh: chart => {
+              
+              chart.data.datasets[0].data.push({
+                x: Date.now(),
+                y: gyro_x_value
+              })
+              chart.data.datasets[1].data.push({
+                x: Date.now(),
+                y: gyro_y_value
+              })
+              chart.data.datasets[2].data.push({
+                x: Date.now(),
+                y: gyro_z_value
+              })
+
+              
+
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
